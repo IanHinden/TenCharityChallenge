921b119d73eb73fe8e4ae87130691d0a7c4fca21
IanHinden <Ian.Hinden@gmail.com>
2018-08-26 01:08:12 -0700

    Added header form and styling for header

--- index.php
+++ index.php
@@ -9,13 +9,25 @@
 <header>
 	<nav>
 		<div class="main-wrapper">
-		
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="selecting.php">Picking a Charity</a></li>
+			</ul>
+			<div class="nav-login">
+				<form>
+					<input type="text" name="uid" placeholder="Username/email">
+					<input type="password" name="pwd" placeholder="Password">
+					<button type="submit" name="submit"></button>
+				</form>
+				<a href="signup.php">Sign up</a>
+			</div>
 		</div>
 	</nav>
 </header>
 
 <section>
-
+	<div>Welcome to the Ten Charity Challenge</div>
 </section>
 
 </body>
