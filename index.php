187e60b08aeadf92e9018ece2ccb0ee01c4b535e
IanHinden <Ian.Hinden@gmail.com>
2018-08-26 00:36:07 -0700

    Added CSS file and linked to index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Ten Charity Challenge</title>
+	<link rel="stylesheet" type="text/cass" href="style.css">
+</head>
+<body>
+
+<header>
+	<nav>
+		<div class="main-wrapper">
+		
+		</div>
+	</nav>
+</header>
+
+<section>
+
+</section>
+
+</body>
+</html>
