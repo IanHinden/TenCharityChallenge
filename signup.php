ce342d480cfc963d56cce4b0c12b4606ffedb618
IanHinden <Ian.Hinden@gmail.com>
2018-08-26 23:29:17 -0700

    Added signup page

---
+++ signup.php
@@ -0,0 +1,15 @@
+<?php
+	include_once 'header.php';
+?>
+
+<section class="main-container">
+	<div class="main-wrapper">
+		<h2>Signup</h2>
+	</div>
+</section>
+<section>
+
+
+<?php
+	include_once 'footer.php';
+?>
