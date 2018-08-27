34c327e4fc2c9f5b87215b09cfee923d7069937c
IanHinden <Ian.Hinden@gmail.com>
2018-08-26 23:29:04 -0700

    Added login text and fixed colors and removed underlines for text in header

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 				<form>
 					<input type="text" name="uid" placeholder="Username/email">
 					<input type="password" name="pwd" placeholder="Password">
-					<button type="submit" name="submit"></button>
+					<button type="submit" name="submit">Login</button>
 				</form>
 				<a href="signup.php">Sign up</a>
 			</div>
--- index.php
+++ index.php
@@ -7,6 +7,18 @@
 		<h2>Welcome to the Ten Charity Challenge </h2>
 	</div>
 </section>
+<section>
+	<div>
+		<h2>Can you volunteer? Ten times in one year?</h2>
+		<p>At the Ten Charity Challenge, we have just one question: can you successfully volunteer ten times in a year? To win, all you need to do serve at a soup kitchen once a month. Or pack lunches for homeless teens. Or drive pets to the vet for veterans...</p>
+	</div>
+		</section>
+<section>
+	<div>	
+		<h2>What is this site?</h2>
+		<p>The purpose of this website is to give you a space to keep track of your volunteer events. This site is not a necessary part of the challenge. In fact, everything site does can be replicated with a pencil and paper. But if you want a place to keep track, leave notes and photos, and share events to involve your friends, we're here for you.<p>
+	</div>
+</section>
 
 <?php
 	include_once 'footer.php';
