9fe2e046d08ad1709af62dcca502d59726260d74
IanHinden <Ian.Hinden@gmail.com>
2018-08-27 09:48:14 -0700

    Added signup form and styling

--- signup.php
+++ signup.php
@@ -5,6 +5,14 @@
 <section class="main-container">
 	<div class="main-wrapper">
 		<h2>Signup</h2>
+		<form class="signup-form">
+			<input type="text" name="first" placeholder="First name">
+			<input type="text" name="last" placeholder="Last name">
+			<input type="text" name="email" placeholder="E-mail">
+			<input type="text" name="uid" placeholder="User name">
+			<input type="password" name="pwd" placeholder="Password">
+			<button type="submit" name="submit">Sign up</
+		</form>
 	</div>
 </section>
 <section>
