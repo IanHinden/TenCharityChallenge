00bf6c02d6862d6378c1638b8fccda7bd4e24d99
IanHinden <Ian.Hinden@gmail.com>
2018-08-31 23:58:05 -0700

    Added login functionality

--- header.php
+++ header.php
@@ -1,3 +1,8 @@
+<?php
+	session_start();
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,7 +20,7 @@
 				<li><a href="selecting.php">Picking a Charity</a></li>
 			</ul>
 			<div class="nav-login">
-				<form>
+				<form action="includes/login.inc.php" method="POST">
 					<input type="text" name="uid" placeholder="Username/email">
 					<input type="password" name="pwd" placeholder="Password">
 					<button type="submit" name="submit">Login</button>
