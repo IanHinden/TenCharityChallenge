143bb00ccaec33f95a92224990b938d1a979d05c
IanHinden <Ian.Hinden@gmail.com>
2018-09-06 23:43:12 -0700

    Added First name greeting to header

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	include_once 'includes/dbh.inc.php'; 
 ?>
 
 
@@ -22,6 +23,7 @@
 			<div class="nav-login">
 				<?php
 					if (isset($_SESSION['u_id'])){
+						echo "Hello, " . $_SESSION["u_first"];
 						echo '<form action="includes/logout.inc.php" method="POST">
 						<button type="submit" name="submit">Logout</button>
 						</form>';
