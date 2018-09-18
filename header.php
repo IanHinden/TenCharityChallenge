bc86aa283389c09c0d8d09cba6ff8fe95277ab0f
IanHinden <Ian.Hinden@gmail.com>
2018-09-18 00:00:09 -0700

    Added Noto Serif JP Google font to header

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 <head>
 	<title>The Ten Charity Challenge</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP" rel="stylesheet">
 </head>
 <body>
 
