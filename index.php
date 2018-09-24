63477b94ac56a5a75fa70cc85c17ee540489ecfd
IanHinden <Ian.Hinden@gmail.com>
2018-09-23 23:30:22 -0700

    Added initial flex-box CSS for main page

--- index.php
+++ index.php
@@ -35,18 +35,30 @@
 			echo 'test';
 		}
 	?>
-<section>
-	<div class="desc">
-		<h2>Can you volunteer? Ten times in one year?</h2>
-		<h3>At the Ten Charity Challenge, we have just one question: can you successfully volunteer ten times in a year? To win, all you need to do serve at a soup kitchen once a month. Or pack lunches for homeless teens. Or drive pets to the vet for veterans...</h3>
-	</div>
-		</section>
-<section>
-	<div>	
-		<h2>What is this site?</h2>
-		<p>The purpose of this website is to give you a space to keep track of your volunteer events. This site is not a necessary part of the challenge. In fact, everything site does can be replicated with a pencil and paper. But if you want a place to keep track, leave notes and photos, and share events to involve your friends, we're here for you.<p>
-	</div>
-</section>
+<div id="main-about">
+	<section id="intro">
+		<div class="desc">
+			<h2>Can you volunteer? Ten times in one year?</h2>
+			<h3>At the Ten Charity Challenge, we have just one question: can you successfully volunteer ten times in a year? To win, all you need to do serve at a soup kitchen once a month. Or pack lunches for homeless teens. Or drive pets to the vet for veterans...</h3>
+		</div>
+		<svg>
+			<text x="5" y="30">A nice rectangle</text>
+		</svg>
+	</section>
+	<section>
+		<div class="purpose">	
+			<div class="purpose-child">
+				<h2>What is this site?</h2>
+				<h3>The purpose of this website is to give you a space to keep track of your volunteer events. This site is not a necessary part of the challenge. In fact, everything site does can be replicated with a pencil and paper. But if you want a place to keep track, leave notes and photos, and share events to involve your friends, we're here for you.</h3>
+			<div class="purpose-child">
+				<h2>Test</h2>
+			</div>
+			<div class="purpose-child">
+				<img src="images/tencharitytest.jpg" alt="Purpose">
+			</div>
+		</div>
+	</section>
+</div>
 
 <?php
 	include_once 'footer.php';
