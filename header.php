62bca06c1fbb340804698b31978ec68eb39ae981
IanHinden <Ian.Hinden@gmail.com>
2018-10-06 22:43:33 -0700

    Added jQuery link and animation for sliding navigation menu on mobile

--- header.php
+++ header.php
@@ -9,6 +9,8 @@
 <head>
 	<title>The Ten Charity Challenge</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="javascript/scripts.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat|Noto+Serif+JP" rel="stylesheet">
 </head>
 <body>
@@ -16,11 +18,18 @@
 <header>
 	<nav>
 		<div class="main-wrapper">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="about.php">About</a></li>
-				<li><a href="selecting.php">Picking a Charity</a></li>
-			</ul>
+		      <span class="menu-icon">
+				<span class="menu-line menu-line-1"></span>
+				<span class="menu-line menu-line-2"></span>
+				<span class="menu-line menu-line-3"></span>
+			</span>
+			<div class="navlink">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About</a></li>
+					<li><a href="selecting.php">Picking a Charity</a></li>
+				</ul>
+			</div>
 			<div class="nav-login">
 				<?php
 					if (isset($_SESSION['u_id'])){
