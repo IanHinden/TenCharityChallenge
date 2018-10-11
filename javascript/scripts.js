8969fdff046beeb50d6201cc9d922bcf4df22d6b
IanHinden <Ian.Hinden@gmail.com>
2018-10-11 00:25:26 -0700

    Added Javascript to slide menu in and out for mobile and transform menu icon

---
+++ javascript/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.menu-icon').click(function() {
+      $('.navlink').toggleClass('navlink-out');
+	  $('.nav-login').toggleClass('nav-login-out');
+	  $('.menu-line-1').toggleClass('menu-line-1-open');
+	  $('.menu-line-2').toggleClass('menu-line-2-open');
+	  $('.menu-line-3').toggleClass('menu-line-3-open');
+    });
+	
+});
