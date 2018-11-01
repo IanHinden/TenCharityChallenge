648e81369d4a0f0bf3a12ff34f4ce1435c0afd4c
IanHinden <Ian.Hinden@gmail.com>
2018-10-31 23:28:10 -0700

    Added initial footer structure for partners, e-mail list, and additional links

--- footer.php
+++ footer.php
@@ -1,5 +1,21 @@
 <div id="footer">
-
+	<section id="partners">
+		<h1>Partners</h1>
+	</section>
+	<section id="footer-links">
+		<div id="mailing-list">
+			<p>Enter your e-mail address for updates</p>
+			<form>
+				<input type="text" name="mailinglist" placeholder="E-mail Address" />
+			</form>
+		</div>
+		<div id="additional-links">
+			<ul>
+				<li>Privacy Policy</li>
+				<li>About</li>
+			</ul>
+		</div>
+	</section>
 </div>
 
 
