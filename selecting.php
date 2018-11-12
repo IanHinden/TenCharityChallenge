900c5ba6d0b62dcfe3db044c6bdada14fdc49eca
IanHinden <Ian.Hinden@gmail.com>
2018-11-11 21:16:38 -0800

    Added selection page

---
+++ selecting.php
@@ -0,0 +1,13 @@
+<?php
+	include_once 'header.php';
+	include_once 'includes/dbh.inc.php';
+?>
+
+<p>This is a selection page</p>
+<br><br><br><br><br>
+
+<p>This is the selection page</p>
+
+<?php
+	include_once 'footer.php';
+?>
