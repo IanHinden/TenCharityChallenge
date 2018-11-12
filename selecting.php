97a2b5eb58862bb6b26b7a5f51d539ff36d74f7d
IanHinden <Ian.Hinden@gmail.com>
2018-11-11 23:08:37 -0800

    Added text advising on how to choose a charity

--- selecting.php
+++ selecting.php
@@ -3,11 +3,15 @@
 	include_once 'includes/dbh.inc.php';
 ?>
 
-<p>This is a selection page</p>
 <br><br><br><br><br>
 
-<p>This is the selection page</p>
+<h1>Advice on Selecting Volunteer Work</h1>
 
+<p>This site was built to make finding volunteer opportunities as simple as possible. Below, you can see resources for searching charities near you. You can also select charity events on your friends pages and add them to your calendar easily.</p>
+<br>
+<p>Oftentimes, you will have to find a volunteer event yourself. Conventional advice says to look for groups in your community or people who have dealt with similar problems.</p>
+<br>
+<p>At 10 Charity Challenge, we actually suggest the inverse. We suggest looking for people of different colors and creeds, statuses and situations. One of the opportunities volunteer work affords us is the chance to learn more about people different from ourselves. When considering a place to start, look outside your bubble!</p>
 <?php
 	include_once 'footer.php';
 ?>
