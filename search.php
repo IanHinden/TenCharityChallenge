b9f2b520af72b21e8681f21cb36b159115e39eee
IanHinden <Ian.Hinden@gmail.com>
2019-01-16 19:47:47 -0800

    Added search page and initial ability to search for first or last name

---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+	include_once 'header.php';
+	include_once 'includes/dbh.inc.php';
+?>
+
+<p>This is a search page</p>
+<br><br><br><br><br>
+
+<form action="" method="post">
+<input type="text" name="search">
+<input type="submit" name="submit" value="Search">
+</form>
+
+<?php
+
+$name = $_POST['search'];
+$result = mysqli_query($conn, "SELECT * FROM users
+    WHERE user_first LIKE '%$name%' OR user_last LIKE '%$name%'");
+
+while ($row = mysqli_fetch_array($result))
+{
+        echo $row['user_first'] . " " . $row['user_last'];
+        echo "<br>";
+}
+    mysqli_close($conn);
+?>
+
+<?php
+	include_once 'footer.php';
+?>
