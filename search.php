571368ef847f37534dc790ae9f9949bde215ebdd
IanHinden <Ian.Hinden@gmail.com>
2019-02-02 00:13:39 -0800

    Fixed search so that user can't search for empty

--- search.php
+++ search.php
@@ -20,32 +20,36 @@ $name = $_POST['search'];
 $result = mysqli_query($conn, "SELECT * FROM users
     WHERE user_first LIKE '%$name%' OR user_last LIKE '%$name%'");
 
-while ($row = mysqli_fetch_array($result))
+	if ($name !=""){
+		while ($row = mysqli_fetch_array($result))
 
-        $usernumber = $row['user_id'];
-		echo $usernumber;
-		
-		echo $row['user_first'] . " " . $row['user_last'];
-		
-		echo '<form action="" method="post">
-		<input type="hidden" name="request">
-		<input type="submit" name="submit" value="Add Friend"';
-		
-		/*
-		$sql = "INSERT INTO relationships (user_one_id, user_two_id, status, action_user_id) VALUES (?,?,?,?);";
-		$stmt = mysqli_stmt_init($conn);
-		if (!mysqli_stmt_prepare($stmt, $sql)){
-			echo "SQL error";
-		} else {
-			mysqli_stmt_bind_param($stmt,"ssss", $current, $usernumber, 1, $current);
-			mysqli_stmt_execute($stmt);
-			header("Location: ../search.php?addfriend=success");
-			exit();
+				$usernumber = $row['user_id'];
+				echo $usernumber;
+				
+				echo $row['user_first'] . " " . $row['user_last'];
+				
+				echo '<form action="" method="post">
+				<input type="hidden" name="request">
+				<input type="submit" name="submit" value="Add Friend">';
+				
+				/*
+				if (isset($_POST['Add Friend'])) {
+					$sql = "INSERT INTO relationships (user_one_id, user_two_id, status, action_user_id) VALUES (?, ?, ?, ?);";
+					$stmt = mysqli_stmt_init($conn);
+					if (!mysqli_stmt_prepare($stmt, $sql)){
+						echo "SQL error";
+					} else {
+						mysqli_stmt_bind_param($stmt,"ssss", $current, $usernumber, 1, $current);
+						mysqli_stmt_execute($stmt);
+						header("Location: ../search.php?addfriend=success");
+						exit();
+					}
+				}	
+				*/
+				
+				echo "<br>";
 		}
-		*/
-		
-        echo "<br>";
-}
+	}
     mysqli_close($conn);
 ?>
 
