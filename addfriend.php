f122335abcdfb226caa01213e11ed58cbb38fe8b
IanHinden <Ian.Hinden@gmail.com>
2019-02-10 12:50:05 -0800

    Added variables from post array

--- addfriend.php
+++ addfriend.php
@@ -1,7 +1,10 @@
 <?php
 	include_once 'header.php';
 	include_once 'includes/dbh.inc.php';
-			
+	
+	$current = $_POST['current'];
+	$usernumber = $_POST['usernumber'];
+	
 	$mysql=mysqli_query($conn, "INSERT INTO relationships (user_one_id, user_two_id, status, action_user_id) VALUES ($current, $usernumber, '1', $current)");
 
 	
