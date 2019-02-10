0c18adbc8d0f69f72f26a472a11a07d32eb1eaa5
IanHinden <Ian.Hinden@gmail.com>
2019-02-10 12:44:57 -0800

    Remove isset conditional

--- addfriend.php
+++ addfriend.php
@@ -2,9 +2,7 @@
 	include_once 'header.php';
 	include_once 'includes/dbh.inc.php';
 			
-				
-	if (isset($_POST['Add Friend'])) {
-		$mysql=mysqli_query($conn, "INSERT INTO relationships (user_one_id, user_two_id, status, action_user_id) VALUES ($current, $usernumber, '1', $current)");
-	}	
+	$mysql=mysqli_query($conn, "INSERT INTO relationships (user_one_id, user_two_id, status, action_user_id) VALUES ($current, $usernumber, '1', $current)");
+
 	
 ?>
