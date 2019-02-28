acd15a296d9bfd5ff51b5c63c9185d55a7ac73d5
IanHinden <Ian.Hinden@gmail.com>
2019-02-28 07:56:36 +0000

    Fix login issue

--- signup.php
+++ signup.php
@@ -5,14 +5,14 @@
 <section class="main-container">
 	<div class="main-wrapper">
 		<h2>Signup</h2>
-		<form class="signup-form" action="includes/signup.inc.php" method="POST" onsubmit="if(document.getElementById('agree').checked) { return true; } else { alert('Please indicate that you have read and agree to the Terms of Service and Privacy Policy'); return false; }">
+		<form class="signup-form" action="includes/signup.inc.php" method="POST">
 			<input type="text" name="first" placeholder="First name">
 			<input type="text" name="last" placeholder="Last name">
 			<input type="text" name="email" placeholder="E-mail">
 			<input type="text" name="uid" placeholder="User name">
 			<input type="password" name="pwd" placeholder="Password">
-		</form>
-			<!-- <button type="submit" name="submit">Sign up</button> -->
+	
+			 <button type="submit" name="submit" value="submit">Sign up</button></form>
 				<button id="show-tos">Submit</button>
 	</div>
 	<div class="confirm">
@@ -24,11 +24,11 @@
 		</div>
 		<p>Terms of Service</p>
 		<p id="terms">I will do my best.</p>
-		<form class="signup-form">
+		<!-- <form class="signup-form" action="includes/signup.inc.php" method="POST" onsubmit="if(document.getElementById('agree').checked) { return true; } else { alert('Please indicate that you have read and agree to the Terms of Service and Privacy Policy'); return false; }">
 		<input type="checkbox" name="checkbox" value="check" id="agree" />
 		<p>I agree to the Terms of Service</p>
-		<button type="submit" name="submit" value="submit" />Sign up</button>
-		</form>
+		<button type="submit" name="submit" value="submit"/>Sign up</button>
+		</form> -->
 	</div>
 	<div class="opaque"></div>	
 			
@@ -36,7 +36,6 @@
 		</form>
 
 </section>
-<section>
 
 
 <?php
