4152f1592c4f98767d3ba94e092108e262b1f604
IanHinden <Ian.Hinden@gmail.com>
2019-03-06 00:25:21 -0800

    Added ability to upload files

--- upload.php
+++ upload.php
@@ -11,5 +11,24 @@ if (isset($_POST['photosubmit'])) {
 	$fileName = $_FILES['profilephoto']['type']
 	
 	$fileExt = explode('.', $fileName);
-	$fileActualExtension = strtolower(end($fileExt));
+	$fileActualExt = strtolower(end($fileExt));
+	
+	$allowed = array('jpg', 'jpeg', 'png', 'pdf');
+	
+	if (in_array($fileActialExt, $allowed)) {
+		if ($fileError === 0) {
+			if ($fileSize < 1000000) {
+				$fileNameNew = uniqid('', true). "." . $fileActualExt;
+				$fileDestination = 'uploads/'. $fileNameNew;
+				move_uploaded_file($fileTmpName, $fileDestination);
+				header("Location: index.php?uploadsuccess");
+			} else {
+				echo "The file you are uploading is too large";
+			}
+		} else {
+			echo "There was an error uploading your file";
+		}
+	} else {
+		echo "You cannot upload files of this type";
+	}
 }
