ad8a75a3d2d92a32372f5cbdeae256437c153542
IanHinden <Ian.Hinden@gmail.com>
2019-03-06 07:48:09 +0000

    Added semicolons to file description statements

--- upload.php
+++ upload.php
@@ -4,11 +4,11 @@ if (isset($_POST['photosubmit'])) {
 	$file = $_FILES['profilephoto'];
 	
 	print_r($file);
-	$fileName = $_FILES['profilephoto']['name']
-	$fileTmpName = $_FILES['profilephoto']['tmp_name']
-	$fileSize = $_FILES['profilephoto']['size']
-	$fileError = $_FILES['profilephoto']['error']
-	$fileName = $_FILES['profilephoto']['type']
+	$fileName = $_FILES['profilephoto']['name'];
+	$fileTmpName = $_FILES['profilephoto']['tmp_name'];
+	$fileSize = $_FILES['profilephoto']['size'];
+	$fileError = $_FILES['profilephoto']['error'];
+	$fileName = $_FILES['profilephoto']['type'];
 	
 	$fileExt = explode('.', $fileName);
 	$fileActualExtension = strtolower(end($fileExt));
