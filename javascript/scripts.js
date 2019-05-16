490647805f0291f924be5d62421b9bd1645b8731
IanHinden <Ian.Hinden@gmail.com>
2019-05-15 23:55:09 -0700

    Added script to connect search box to results on map

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -62,6 +62,48 @@ function createMap() {
 	} else {
 		handleLocationError('No geolocation available', map.center());
 	}
+	
+	var input = document.getElementById("search");
+	var searchBox = new google.maps.places.SearchBox(input);
+	
+	map.addListener('bounds_changed', function() {
+		searchBox.setBounds(map.getBounds());
+	});
+	
+	var markers = [];
+	
+	searchBox.addListener('places_changed', function() {
+		var places = searchBox.getPlaces();
+		
+		if(places.length === 0)
+			return;
+		
+		markers.forEach(function (m) {m.setMap(null); });
+		markers = [];
+	
+		var bounds = new google.maps.LatLngBounds();
+	
+		places.forEach(function (p){
+			if (!p.geometry)
+				return;
+			
+			markers.push(new google.maps.Marker({
+					map: map,
+					title: p.name,
+					position: p.geometry.location
+			}));
+			
+			if (p.geometry.viewport)
+				bounds.union(p.geometry.location);
+			else
+				bounds.extend(p.geometry.location);
+		});
+		
+		map.fitBounds(bounds);
+	});	
+});
+	
+	
 }
 
 function handleLocationError (content, position) {
