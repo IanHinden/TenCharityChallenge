101a2dfa6f7f0376b166cf34df8f850af227295a
IanHinden <Ian.Hinden@gmail.com>
2019-05-22 23:48:57 -0700

    Added initial user page

---
+++ user.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="User page">
+		<meta name=viewport content="width=device-width, initial-scale=1">
+		<title>Profile Page</title>
+	</head>
+	<body>
+	
+		<?php
+			$userId = $_GET['id'];
+			
+			echo "User ID is: " $userId;
+		?>
+	</body>
+</html>
