1132a5ab811d63ed96411cbd22f7e5c4d30c76b9
root <root@ip-172-31-31-4.us-west-2.compute.internal>
2019-07-14 15:13:59 +0000

    Signup now sends e-mail on new signup

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,9 +1,45 @@
 <?php
 
+require '../vendor/autoload.php';
+
+use Aws\Ses\SesClient;
+use Aws\Exception\AwsException;
+
 if (isset($_POST['submit'])) {
 	
 	include 'dbh.inc.php';
 	
+	$SesClient = new SesClient([
+    //'profile' => 'default',
+    'version' => '2010-12-01',
+    'region'  => 'us-west-2',
+    'credentials' => array(
+    'key' => 'AKIAYNFZXG2VWP4ZBTXK',
+    'secret' => 'NOR5clO0qSCp6D+t7BdiR5zLN3CnXbDl0wNekbST'
+    ),
+]);
+
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+$sender_email = 'donotreply@tencharitychallenge.com';
+
+// Replace these sample addresses with the addresses of your recipients. If
+// your account is still in the sandbox, these addresses must be verified.
+$recipient_emails = ['ian.hinden@gmail.com'];
+
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment the following variable, and the
+// 'ConfigurationSetName' => $configuration_set argument below.
+$configuration_set = 'ConfigSet';
+
+$subject = 'Amazon SES test (AWS SDK for PHP)';
+$plaintext_body = 'This email was sent with Amazon SES using the AWS SDK for PHP.' ;
+$html_body =  '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
+              '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+              'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+              'AWS SDK for PHP</a>.</p>';
+$char_set = 'UTF-8';
+
 	$first = $_POST['first'];
 	$last = $_POST['last'];
 	$email = $_POST['email'];
@@ -54,6 +90,46 @@ if (isset($_POST['submit'])) {
 								mysqli_query($conn, $sqlImg);
 							}
 						}
+
+try {
+    $emailResult = $SesClient->sendEmail([
+        'Destination' => [
+            'ToAddresses' => $recipient_emails,
+        ],
+        'ReplyToAddresses' => [$sender_email],
+        'Source' => $sender_email,
+        'Message' => [
+          'Body' => [
+              'Html' => [
+                  'Charset' => $char_set,
+                  'Data' => $html_body,
+              ],
+              'Text' => [
+                  'Charset' => $char_set,
+                  'Data' => $plaintext_body,
+              ],
+          ],
+          'Subject' => [
+              'Charset' => $char_set,
+              'Data' => $subject,
+          ],
+        ],
+        // If you aren't using a configuration set, comment or delete the
+        // following line
+        //'ConfigurationSetName' => $configuration_set,
+    ]);
+    $messageId = $emailResult['MessageId'];
+    echo("Email sent! Message ID: $messageId"."\n");
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+    echo "\n";
+}
+
+
+
+
 						header("Location: ../signup.php?signup=success");
 						exit();
 					}
