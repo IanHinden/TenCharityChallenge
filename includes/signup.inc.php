a6cfdf2d068b2633d59b90597bf4b9e09752422a
root <root@ip-172-31-31-4.us-west-2.compute.internal>
2019-07-18 09:41:28 +0000

    Edited subject of welcome e-mail and fixed spacing on e-mail function

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -32,8 +32,8 @@ $recipient_emails = ['ian.hinden@gmail.com'];
 // 'ConfigurationSetName' => $configuration_set argument below.
 $configuration_set = 'ConfigSet';
 
-$subject = 'Amazon SES test (AWS SDK for PHP)';
-$plaintext_body = 'This email was sent with Amazon SES using the AWS SDK for PHP.' ;
+$subject = 'Welcome to the Ten Charity Challenge!';
+$plaintext_body = 'Please confirm your e-mail address to receive updates and reminders.' ;
 $html_body =  '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
               '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
               'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
@@ -91,41 +91,41 @@ $char_set = 'UTF-8';
 							}
 						}
 
-try {
-    $emailResult = $SesClient->sendEmail([
-        'Destination' => [
-            'ToAddresses' => $recipient_emails,
-        ],
-        'ReplyToAddresses' => [$sender_email],
-        'Source' => $sender_email,
-        'Message' => [
-          'Body' => [
-              'Html' => [
-                  'Charset' => $char_set,
-                  'Data' => $html_body,
-              ],
-              'Text' => [
-                  'Charset' => $char_set,
-                  'Data' => $plaintext_body,
-              ],
-          ],
-          'Subject' => [
-              'Charset' => $char_set,
-              'Data' => $subject,
-          ],
-        ],
+						try {
+    							$emailResult = $SesClient->sendEmail([
+						        'Destination' => [
+						            'ToAddresses' => $recipient_emails,
+						        ],
+						        'ReplyToAddresses' => [$sender_email],
+						        'Source' => $sender_email,
+						        'Message' => [
+						          'Body' => [
+						              'Html' => [
+						                  'Charset' => $char_set,
+						                  'Data' => $html_body,
+						              ],
+						              'Text' => [
+						                  'Charset' => $char_set,
+						                  'Data' => $plaintext_body,
+						              ],
+						          ],
+						          'Subject' => [
+						              'Charset' => $char_set,
+						              'Data' => $subject,
+						          ],
+						        ],
         // If you aren't using a configuration set, comment or delete the
         // following line
         //'ConfigurationSetName' => $configuration_set,
-    ]);
-    $messageId = $emailResult['MessageId'];
-    echo("Email sent! Message ID: $messageId"."\n");
-} catch (AwsException $e) {
+						    ]);
+						    $messageId = $emailResult['MessageId'];
+						    echo("Email sent! Message ID: $messageId"."\n");
+							} catch (AwsException $e) {
     // output error message if fails
-    echo $e->getMessage();
-    echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
-    echo "\n";
-}
+							    echo $e->getMessage();
+							    echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+							    echo "\n";
+							}
 
 
 
