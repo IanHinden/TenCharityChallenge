07f5b193f29c445547e9508330d8f023ab75de1c
Ian Hinden <Ian.Hinden@gmail.com>
2019-09-05 06:32:32 +0000

    Made sure friend requests have duplicate data and friend confirmation can find correct request

--- addfriend.php
+++ addfriend.php
@@ -4,8 +4,16 @@
 	
 	$current = $_SESSION['u_id'];
 	$usernumber = $_POST['usernumber'];
+
+	if ($current < $usernumber){
+		$lower = $current;
+		$higher = $usernumber;
+	} else {
+		$lower = $usernumber;
+		$higher = $current;
+	}
 	
-	$mysql=mysqli_query($conn, "INSERT INTO relationships (user_one_id, user_two_id, status, action_user_id) VALUES ($current, $usernumber, '0', $current)");
+	$mysql=mysqli_query($conn, "INSERT INTO relationships (user_one_id, user_two_id, status, action_user_id) VALUES ($lower, $higher, '0', $current)");
 	
 	
 ?>
--- confirmfriend.php
+++ confirmfriend.php
@@ -4,8 +4,16 @@
 	
 	$current = $_SESSION['u_id'];
 	$userid = $_POST['userid'];
+
+	if ($current < $userid){
+                $lower = $current;
+                $higher = $userid;
+        } else {
+                $lower = $userid;
+                $higher = $current;
+        }
 	
-	$mysql=mysqli_query($conn, "UPDATE relationships SET status = 1, action_user_id = $current WHERE user_one_id = $current");
+	$mysql=mysqli_query($conn, "UPDATE relationships SET status = 1, action_user_id = $current WHERE user_one_id = $lower AND user_two_id = $higher");
 	
 	
 ?>
