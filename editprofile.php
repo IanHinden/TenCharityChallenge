081858093479110caae2d017bba88ca999168b63
Ian Hinden <Ian.Hinden@gmail.com>
2019-10-26 08:01:27 +0000

    Text from database profile auto-populates field profile field

--- editprofile.php
+++ editprofile.php
@@ -17,11 +17,20 @@
 			if (isset($_SESSION['u_id'])){
                         	$id = $_SESSION['u_id'];
 
-				echo '<br><br><br><br>';
-                        	echo "User ID is: " . $id;
+				//Retreive user details
+				$sql = "SELECT * FROM profile WHERE user_id = '".$_SESSION['u_id']."';";
+                        	$result = mysqli_query($conn, $sql);
+				$resultCheck = mysqli_num_rows($result);
 
+				echo '<br><br><br><br><br>';
+
+				for ($set = array (); $row = mysqli_fetch_assoc($result); $set[] = $row);
+                                foreach ($set as $item){
+                                        $aboutMeText = $item['about_me'];
+                                }
+				echo $aboutMeText;
 				echo '<form action="includes/aboutme.inc.php" method="post">
-				About Me: <input type="text" name="aboutme" id="aboutme" readonly="readonly"><br>
+				About Me: <input type="text" value="'.$aboutMeText.'" name="aboutme" id="aboutme" readonly="readonly"><br>
 				<p id="editaboutme">Edit</p>
 				<input type="submit">
 				</form>';
