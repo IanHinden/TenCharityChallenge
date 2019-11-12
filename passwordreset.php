07538ac00385887949ce2d0d42c07ad3993d1471
Ian Hinden <Ian.Hinden@gmail.com>
2019-11-12 07:08:46 +0000

    Password reset page checks for existence of selector and validator

--- passwordreset.php
+++ passwordreset.php
@@ -1,12 +1,22 @@
 <?php
         include_once 'header.php';
+	include_once 'includes/dbh.inc.php';
 ?>
 
 <?php
+$selector = $_GET['selector'];
+$validator = $_GET['validator'];
 
-echo '<br><br><br><br><form class="password-reset-form" action="includes/passwordreset.inc.php" method="POST">
-			<p>Please enter your e-mail address and submit. Then follow the instructions in the e-mail to reset your password.</p>
-                        <input type="text" name="email" placeholder="E-mail Address"><br>
-			<button type="submit" value="submit" name="submit">Submit</button></form>';
+echo '<br><br><br><br>';
 
+if(isset($selector)) {
+	echo "Selector is: " . $selector;
+	echo "Token is: " . $validator;
+} else {
+
+echo '<form class="password-reset-form" action="includes/passwordreset.inc.php" method="POST">
+	<p>Please enter your e-mail address and submit. Then follow the instructions in the e-mail to reset your password.</p>
+        <input type="text" name="email" placeholder="E-mail Address"><br>
+	<button type="submit" value="submit" name="submit">Submit</button></form>';
+}
 ?>
