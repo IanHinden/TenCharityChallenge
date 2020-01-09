2f823a83d2ff1276ed3a45ba70a1b7e3326002c5
Ian Hinden <Ian.Hinden@gmail.com>
2020-01-09 06:58:08 +0000

    Event Info is added to pdf

--- report.php
+++ report.php
@@ -1,7 +1,19 @@
 <?php
+session_start();
+include_once 'includes/dbh.inc.php';
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $mpdf = new \Mpdf\Mpdf(['tempDir' => __DIR__ . '/tmp']);
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output('myfile.pdf', 'D');
+
+$sql = "SELECT * FROM events INNER JOIN eventrelationships ON events.event_id = eventrelationships.event_id WHERE user_id = '".$_SESSION['u_id']."' AND completed = 1;";
+$result = mysqli_query($conn, $sql);
+$totalEvents = mysqli_num_rows($result);
+
+if ($totalEvents > 0) {
+	while ($row = mysqli_fetch_assoc($result)) {
+        	$mpdf->WriteHTML('<h1>' . $row["event_info"] . '</h1>');
+        }
+}
+
+$mpdf->Output('VolunteerReport.pdf', 'D');
