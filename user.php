8d23074d8e4ffe4f51ae0db19fc7d25628d3c4fc
Ian Hinden <Ian.Hinden@gmail.com>
2020-01-10 07:39:46 +0000

    Initial logic to determine if relationship exists and which user made most recent change

--- user.php
+++ user.php
@@ -1,5 +1,6 @@
 <?php
 	include_once 'includes/dbh.inc.php';
+	session_start();
 ?>
 
 <!DOCTYPE html>
@@ -13,11 +14,41 @@
 	<body>
 	
 		<?php
-			$userId = $_GET['id'];
+			$profileId = $_GET['id'];
 			
-			echo "User ID is: " . $userId;
+			if(isset($_SESSION['u_id'])) {
+                                $userId = $_SESSION['u_id'];
+
+				//Determine which number is smaller
+                        	if ($userId < $profileId){
+                                	$lower = $userId;
+                                	$higher = $profileId;
+                        	} else {
+                                	$lower = $profileId;
+                                	$higher = $userId;
+                        	}
+
+				$sql = "SELECT * FROM relationships WHERE user_one_id = '".$lower."' AND user_two_id = '".$higher."';";
+                        	$result = mysqli_query($conn, $sql);
+                        	$resultCheck = mysqli_num_rows($result);
+
+				if ($resultCheck > 0) {
+                                	while ($row = mysqli_fetch_assoc($result)) {
+                                        	if ($row['action_user_id'] == $userId ) {
+                                                	echo "This logged in user requested a change";
+                                        	} else {
+                                                	echo "The user that owns this profile requested a change";
+                                        	}
+                                	}
+                        	} else {
+                                	echo "These users have no relationships";
+                        	}
+
+                        } else {
+                                echo "There is no user";
+                    	}
 			
-			$sql = "SELECT * FROM users WHERE user_id = '".$userId."';";
+			$sql = "SELECT * FROM users WHERE user_id = '".$profileId."';";
 			$result = mysqli_query($conn, $sql);
 			$resultCheck = mysqli_num_rows($result);
 			
