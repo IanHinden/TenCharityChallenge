17623f1405ae1bdeae3eefc27134d37e2cbf9ad8
Ian Hinden <Ian.Hinden@gmail.com>
2020-02-11 07:32:47 +0000

    Fixed issue where CSS would not apply on mobile

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="javascript/scripts.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat|Noto+Serif+JP" rel="stylesheet"></link>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
 <body>
 
