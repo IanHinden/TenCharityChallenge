008145173b927127133173d20a7539331a45dc05
Ian Hinden <Ian.Hinden@gmail.com>
2020-02-21 08:28:33 +0000

    Centered items in header horizontally

--- header.php
+++ header.php
@@ -25,32 +25,34 @@
 				<span class="menu-line menu-line-3"></span>
 			</span>
 			<div class="navlink">
-				<p>Ten Charity Challenge</p>
-			</div>
-			<div class="nav-login">
-				<?php
-					if (isset($_SESSION['u_id'])){
-						echo '<p id="headergreeting">Hello, ' . $_SESSION['u_first'] . '</p>';
-						echo '<form action="includes/logout.inc.php" method="POST">
-						<button type="submit" name="submit">Logout</button>
-						</form>';
-					} else {
-						echo '<div id="loginbox">
-							<form action="includes/login.inc.php" method="POST">
-							<table>
-								<tr>
-									<td><input type="text" name="uid" placeholder="Username/email"></input></td>
-									<td><input type="password" name="pwd" placeholder="Password"></input></td>
-									<td><button type="submit" name="submit">Login</button></td>
-								</tr>
-								<tr>
-									<td></td>
-									<td><a href="https://tencharitychallenge.com/passwordreset.php">Forgot your password?</a></td>
-									<td></td>
-								</tr>
-							</table>
-						</div>
-							</form>';
+				<div id="header-items">
+					<p>Ten Charity Challenge</p>
+					<div class="nav-login">
+						<?php
+							if (isset($_SESSION['u_id'])){
+								echo '<p id="headergreeting">Hello, ' . $_SESSION['u_first'] . '</p>';
+								echo '<form action="includes/logout.inc.php" method="POST">
+								<button type="submit" name="submit">Logout</button>
+								</form>';
+							} else {
+								echo '<div id="loginbox">
+									<form action="includes/login.inc.php" method="POST">
+									<table>
+										<tr>
+											<td><input type="text" name="uid" placeholder="Username/email"></input></td>
+											<td><input type="password" name="pwd" placeholder="Password"></input></td>
+											<td><button type="submit" name="submit">Login</button></td>
+										</tr>
+										<tr>
+											<td></td>
+											<td><a href="https://tencharitychallenge.com/passwordreset.php">Forgot your password?</a></td>
+											<td></td>
+										</tr>
+									</table>
+								</div>
+									</form>
+					</div>
+				</div>';
 					}
 				?>
 			</div>
