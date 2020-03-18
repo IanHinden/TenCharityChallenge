0b6d63ff5efd7562810104cc1ec9f84bf491cd69
Ian Hinden <Ian.Hinden@gmail.com>
2020-03-18 06:56:51 +0000

    Fixed issue where www would throw CORS error with event xml

--- eventsearch.php
+++ eventsearch.php
@@ -1,6 +1,10 @@
 <?php
 	include_once 'header.php';
 	include_once 'includes/dbh.inc.php';
+
+	if($_SERVER['HTTP_HOST'] == "www.tencharitychallenge.com"){
+		header("Location: http://tencharitychallenge.com/eventsearch.php");
+	}
 ?>
 
 
