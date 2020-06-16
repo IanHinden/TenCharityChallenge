e5bf3fa4e840e382ef50a10f9d905ceeb897b3e4
Ian Hinden <Ian.Hinden@gmail.com>
2020-06-16 03:09:20 +0000

    Added fuller list of volunteer categories

--- addevent.php
+++ addevent.php
@@ -15,7 +15,27 @@
 						<input type="datetime-local" name="datetime-local">
 	        				<select name="cause">
 	        					<option value="Animals">Animals</option>
-	                				<option value="Food Bank">Food Bank</option>
+							<option value="Arts">Arts</option>
+							<option value="Assisted Living">Assisted Living</option>
+							<option value="Chemical Dependency Services">Chemical Dependency Services</option>
+							<option value="Community Development">Community Development</option>
+							<option value="Criminal Rehabilitation">Criminal Rehabilitation</option>
+							<option value="Crisis Lines">Crisis Lines</option>
+							<option value="Services for the Developmentally Challenged">Services for the Developmentally Challenged</option>
+	                				<option value="Domestic Abuse and Protective Services">Domestic Abuse and Protective Services</option>
+							<option value="Environmental Issues">Environmental Issues</option>
+							<option value="Family Counseling">Family Counseling</option>
+							<option value="Food Bank">Food Bank</option>
+							<option value="Health and Fitness">Health and Fitness</option>
+							<option value="Health Care Services">Health Care Services</option>
+							<option value="History">History</option>
+							<option value="Hospice Services">Hospice Services</option>
+							<option value="Juvenile Services">Juvenile Services</option>
+							<option value="Literary Services">Literary Services</option>
+							<option value="Mental Health Services">Mental Health Services</option>
+							<option value="Multicultural Services">Multicultural Services</option>
+							<option value="Senior Citizen Services">Senior Citizen Services</option>
+							<option value="Vocational Assistance">Vocational Assistance</option>
 							<option value="Other">Other</option>
 						</select>
 						<input type="hidden" id="lat" name="lat" step="any">
--- eventsearch.php
+++ eventsearch.php
@@ -17,8 +17,28 @@
 		<input type="date" id="toDate" name="toDate"></input>
 		<select id="cause">
 			<option value="All">All</option>
-			<option value="Animals">Animals</option>
-			<option value="Food Bank">Food Bank</option>
+			<option value="Arts">Arts</option>
+                       	<option value="Assisted Living">Assisted Living</option>
+                       	<option value="Chemical Dependency Services">Chemical Dependency Services</option>
+                        <option value="Community Development">Community Development</option>
+                        <option value="Criminal Rehabilitation">Criminal Rehabilitation</option>
+                        <option value="Crisis Lines">Crisis Lines</option>
+                        <option value="Services for the Developmentally Challenged">Services for the Developmentally Challenged</option>
+                        <option value="Domestic Abuse and Protective Services">Domestic Abuse and Protective Services</option>
+                        <option value="Environmental Issues">Environmental Issues</option>
+                        <option value="Family Counseling">Family Counseling</option>
+                        <option value="Food Bank">Food Bank</option>
+                        <option value="Health and Fitness">Health and Fitness</option>
+                        <option value="Health Care Services">Health Care Services</option>
+                        <option value="History">History</option>
+                        <option value="Hospice Services">Hospice Services</option>
+                        <option value="Juvenile Services">Juvenile Services</option>
+                        <option value="Literary Services">Literary Services</option>
+                        <option value="Mental Health Services">Mental Health Services</option>
+                        <option value="Multicultural Services">Multicultural Services</option>
+                        <option value="Senior Citizen Services">Senior Citizen Services</option>
+                        <option value="Vocational Assistance">Vocational Assistance</option>
+                        <option value="Other">Other</option>
 		</select>
 		<input id="refreshMap" type="button" value="Search" onclick="initMap();" />
 
