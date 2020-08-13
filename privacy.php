8d23b4d8f97a3c7e5618b7f18cf5e39df5cdf6aa
Ian Hinden <Ian.Hinden@gmail.com>
2020-08-13 04:20:16 +0000

    Initial privacy policy information regarding what is collected

--- privacy.php
+++ privacy.php
@@ -8,15 +8,20 @@
 
 		echo '<div id="passwordresetcontent">
 			<div id="confirmationtext">';
-			$selector = $_GET['selector'];
-			$validator = $_GET['validator'];
-			
-			echo "Privacy Policy";
+			echo 'Privacy Policy';
+			echo '<div id="infocollected">
+				<p class="privacyheader">What Information We Collect</p>
+				<ul class="privacylist">
+					<li>Name, Username, E-mail, and Password</li>
+					<li>Relationships with other users, including friendships and block requests</li>
+					<li>User submitted content including event information and images</li>
+					<li>Search parameters through friend and event search engines</li>
+					<li>Location of user through browser with permission</li>
+				</ul>
+				<p class="privacyheader">What We Do with the Information We Collect</p>
+			</div>';
 		echo '</div>
 			</div>';
 		?>
 	</body>
 
-<?php
-	include_once 'footer.php';
-?>
