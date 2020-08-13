f5da4dafd0ce6eabc2b59b8b371381b3520e83a8
Ian Hinden <Ian.Hinden@gmail.com>
2020-08-13 21:48:41 +0000

    More privacy policy text

--- privacy.php
+++ privacy.php
@@ -6,10 +6,14 @@
 	
 		<?php
 
-		echo '<div id="passwordresetcontent">
+		echo '<div id="privacycontent">
 			<div id="confirmationtext">';
 			echo 'Privacy Policy';
-			echo '<div id="infocollected">
+			echo '<div id="infocollected">';
+			echo	'<p id="updateddate" class="privacyheader">(Last updated 8/13/2020)</p>
+				<p class="privacyheader">Ten Charity Challenge, LLC ("us", "we", or "our") operates https://tencharitychallenge.com (the
+					"Site"). This page informs you of our policies regarding the collection, use and disclosure of
+					Personal Information we receive from users of the Site.</p>
 				<p class="privacyheader">What Information We Collect</p>
 				<ul class="privacylist">
 					<li>Name, Username, E-mail, and Password</li>
@@ -19,6 +23,14 @@
 					<li>Location of user through browser with permission</li>
 				</ul>
 				<p class="privacyheader">What We Do with the Information We Collect</p>
+				<ul class="privacylist">
+					<li>Account creation and login purposes.</li>
+					<li>Determine which users can view each other\'s pages, content, and event attendance.</li>
+					<li>Display event information for the public to view.</li>
+					<li>Used to display relevant users and events.</li>
+					<li>Verify that users have completed events.</li>
+					<li>Tally completed hours and inspired hours.</li>
+				</ul>
 			</div>';
 		echo '</div>
 			</div>';
